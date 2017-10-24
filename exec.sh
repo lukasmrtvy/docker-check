6606216affe23edea6ba17857c753f595ac90de5
Lukas M <lukas.mrtvy@gmail.com>
2017-10-24 21:05:00 +0200

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
+
+# types
+# https://api.github.com/repos/xbmc/xbmc/releases/latest
+# https://api.github.com/repos/SickRage/SickRage/commits/master
+# https://api.github.com/repos/mopidy/mopidy/tags
+
 #curl -s --unix-socket /var/run/docker.sock http:/v1.30/containers/json | jq '.[]' >
 #curl -s https://api.github.com/repos/xbmc/xbmc/releases/latest |   jq --raw-output .tag_name
 
 command=`curl -s --unix-socket /var/run/docker.sock http:/v1.30/containers/json | jq -r '.[].Id'`
 
-
 #curl -s --unix-socket /var/run/docker.sock http:/v1.30/containers/${id}/json | jq  '.Config.ExposedPorts | keys'
-
 
 #command1=`curl -s --unix-socket /var/run/docker.sock http:/v1.30/containers/json | jq -r '.[].Labels.name'`
 #command2=`curl -s --unix-socket /var/run/docker.sock http:/v1.30/containers/json | jq -r '.[].Labels.version'`
-
 
 #curl -s --unix-socket /var/run/docker.sock http:/v1.30/containers/d91bd7ef81886b6f2c22da6698b7e1715571b0f1d4e4256738b221b7f3b8230c/json | jq  -r  -c '.NetworkSettings.Ports'
 
@@ -34,11 +37,9 @@ cat << 'EOF' >> index.html
   <table class="table table-bordered .table-condensed">
     <thead>
       <tr>
-        <th>1</th>
-        <th>2</th>
-        <th>3</th>
-        <th>4</th>
-        <th>5</th>
+        <th>Name</th>
+        <th>Current version</th>
+        <th>Latest version</th>
       </tr>
     </thead>
     <tbody>
@@ -49,37 +50,51 @@ for p in $command
 do
   getstate=`curl -s --unix-socket /var/run/docker.sock http:/v1.30/containers/${p}/json | jq -r '.State.Status'`
   getname=`curl -s --unix-socket /var/run/docker.sock http:/v1.30/containers/${p}/json | jq -r '.Name'`
-  #getports=`curl -s --unix-socket /var/run/docker.sock http:/v1.30/containers/${p}/json | jq  -r  '.Config.ExposedPorts | keys []'`
-  getports=`curl -s --unix-socket /var/run/docker.sock http:/v1.30/containers/${p}/json | jq  -r  '.NetworkSettings.Ports[][].HostPort'`
-  getgiturl=`curl -s --unix-socket /var/run/docker.sock http:/v1.30/containers/${p}/json | jq -r '.[].Labels.url'`
+#  getports=`curl -s --unix-socket /var/run/docker.sock http:/v1.30/containers/${p}/json | jq  -r  '.Config.ExposedPorts | keys []'`
+#  getports=`curl -s --unix-socket /var/run/docker.sock http:/v1.30/containers/${p}/json | jq  -r  '.NetworkSettings.Ports[][].HostPort'`
+  getgiturl=`curl -s --unix-socket /var/run/docker.sock http:/v1.30/containers/${p}/json | jq -r '.Config.Labels.url'`
+
+
+if [ $(echo "${getgiturl}" |  sed 's:.*/::') = "master"  ]; then
+  echo "master"
+  getgitversion=`curl -s ${getgiturl} | jq -r '.sha'`
+elif [ $(echo "${getgiturl}" |  sed 's:.*/::') = "tags"  ]; then
+  echo "tags"
+  getgitversion=`curl -s ${getgiturl} | jq -r '.[0].name'`
+elif [ $(echo "${getgiturl}" |  sed 's:.*/::') = "latest"  ]; then
+  echo "latest"
+  #getgitversion=`curl -s ${getgiturl} | jq -r '.[0].name'`
   getgitversion=`curl -s ${getgiturl} | jq --raw-output .tag_name`
-  getlocalversion=`curl -s --unix-socket /var/run/docker.sock http:/v1.30/containers/${p}/json | jq -r '.[].Labels.version'`
+fi
+
+  getlocalversion=`curl -s --unix-socket /var/run/docker.sock http:/v1.30/containers/${p}/json | jq -r '.Config.Labels.version'`
+
+
+if [ ${getgiturl} = "null" ] || [ ${getlocalversion} = "null" ]; then
+        getgitversion=$(echo "Not available")
+        getlocalversion=$(echo "Not available")
+fi
 
     echo "<tr>" >> index.html
-#    echo "<th> ${p} </th> " >> index.html
-    echo "<th> ${getname} </th> " >> index.html
-    echo "<th> ${getstate} </th> " >> index.html
-    echo "<th> ${getports}  </th>" >> index.html
-    echo "<th> ${getlocalversion}  </th>" >> index.html
-    echo "<th> ${getgitversion}  </th>" >> index.html
+#    echo "<td> ${p} </td> " >> index.html
+    echo "<td> ${getname} </td> " >> index.html
+#    echo "<th> ${getstate} </td> " >> index.html
+#    echo "<th> ${getports}  </td>" >> index.html
+    echo "<td> ${getlocalversion}  </td>" >> index.html
+    echo "<td> ${getgitversion}  </td>" >> index.html
 
     echo "</tr>" >> index.html
 done
 
 cat << 'EOF' >> index.html
-</tbody>
-</table>
-</div>
-</div>
 
-</body>
+        </tbody>
+      </table>
+    </div>
+   </div>
+  </body>
 </html>
+
 EOF
 
-
-#if [ ${p}  ];
-#then
-#test=`curl -s ${p} | jq --raw-output .tag_name`
-#else
-#test=`curl -s ${p} | jq --raw-output .tag_name`
-#fi
+# echo "done"
