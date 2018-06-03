39bdc755f1a3829a4d22fcdc6b38661dc49502f8
Lukas M <lukas.mrtvy@gmail.com>
2018-06-03 17:38:00 +0200

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,62 +1,37 @@
-#!/bin/sh
+#!/bin/bash
 
-command=`curl -s --unix-socket /var/run/docker.sock http:/v1.30/containers/json | jq -r '.[].Id'`
+api_version=v1.30
 
-echo `date` > index.html
+command=`curl -s --unix-socket /var/run/docker.sock http:/$api_version/containers/json | jq -r '.[].Id'`
 
-cat << 'EOF' >> index.html
-<!DOCTYPE html>
-<html>
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-<body>
-<div class="container">
-  <h2>Docker version table</h2>
-  <div class="table-responsive">
-  <table class="table table-bordered .table-condensed">
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Current version</th>
-        <th>Latest version</th>
-      </tr>
-    </thead>
-    <tbody>
-EOF
+result="result.json"
+echo '{"data":[]}' > "$result"
+
+if [ ${username+set} ] && [ ${token+set} ]; then
+        auth=$(-u ${username}:${token} )
+else 
+        auth=""
+fi
+
 for p in $command
 do
-  getstate=`curl -s --unix-socket /var/run/docker.sock http:/v1.30/containers/${p}/json | jq -r '.State.Status'`
-  getname=`curl -s --unix-socket /var/run/docker.sock http:/v1.30/containers/${p}/json | jq -r '.Name'`
-  getgiturl=`curl -s --unix-socket /var/run/docker.sock http:/v1.30/containers/${p}/json | jq -r '.Config.Labels.url'`
+  j=$(curl -s --unix-socket /var/run/docker.sock http:/$api_version/containers/${p}/json)
+  name=$(jq -r '.Name' <<< "$j")
+  state=$(jq -r '.State.Status' <<< "$j")
+  giturl=$(jq -r '.Config.Labels.url' <<< "$j")
+  local_version=$(jq -r '.Config.Labels.version' <<< "$j")
+  k=$(echo ${giturl} | awk -F'github.com/' '{print $2}' | sed -e 's#/$##')
+  latest_tag=$(curl -s ${auth} https://api.github.com/repos/$k/tags | jq -r '.[0].name' |xargs  )
+  latest_commit=$(curl -s ${auth} https://api.github.com/repos/$k/commits | jq -r '.[0].sha' |xargs  )
+  latest_release=$(curl -s ${auth} https://api.github.com/repos/$k/releases/latest | jq -r ' .name' )
 
-if [ $(echo "${getgiturl}" |  sed 's:.*/::') = "master"  ]; then
-  getgitversion=`curl -s ${getgiturl} | jq -r '.sha'`
-elif [ $(echo "${getgiturl}" |  sed 's:.*/::') = "tags"  ]; then
-  getgitversion=`curl -s ${getgiturl} | jq -r '.[0].name'`
-elif [ $(echo "${getgiturl}" |  sed 's:.*/::') = "latest"  ]; then
-  getgitversion=`curl -s ${getgiturl} | jq --raw-output .tag_name`
-fi
-  getlocalversion=`curl -s --unix-socket /var/run/docker.sock http:/v1.30/containers/${p}/json | jq -r '.Config.Labels.version'`
-if [ ${getgiturl} = "null" ] || [ ${getlocalversion} = "null" ]; then
-        getgitversion=$(echo "Not available")
-        getlocalversion=$(echo "Not available")
-fi
-    echo "<tr>" >> index.html
-    echo "<td> ${getname} </td> " >> index.html
-    echo "<td> ${getlocalversion}  </td>" >> index.html
-    echo "<td> ${getgitversion}  </td>" >> index.html
-    echo "</tr>" >> index.html
+jq --arg arg_name "$name" \
+  --arg arg_state "$state" \
+  --arg arg_giturl "$giturl" \
+  --arg arg_localversion "$local_version" \
+  --arg arg_latestrelease "$latest_release" \
+  --arg arg_latesttag "$latest_tag" \
+  --arg arg_latestcommit "$latest_commit" \
+        '.data += [[ $arg_name, $arg_state, $arg_giturl, $arg_localversion, $arg_latestrelease, $arg_latesttag, $arg_latestcommit ]]' "$result" | sponge "$result"
+
 done
-cat << 'EOF' >> index.html
-        </tbody>
-      </table>
-    </div>
-   </div>
-  </body>
-</html>
-EOF
-
